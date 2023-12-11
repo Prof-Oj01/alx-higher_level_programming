d178de17f561eef017c314e60113298d2dc2f8bc
Prof-Oj01 <ojoofe20@gmail.com>
2023-12-11 07:32:17 -0800

    checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,44 +4,47 @@
 
 /**
  * is_palindrome - function that determines whether a string is a palidrone
- * @listint_t: parameter
- * @*head: pointer to head of s..linked list
+ * @head: pointer to head of s..linked list
  *
- * Return 0 if not palndrome, else 1 if it is a palindrome
+ * Return: 0 if not palndrome, else 1 if it is a palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head, *prev = NULL;
+	listint_t *slow = *head, *fast = *head, *prev = NULL;
 
-    /* Check if the list is empty or has only one element */
-    if (!*head || !(*head)->next) {
-        return (1);
-    }
+	/* Check if the list is empty or has only one element */
+	if (!*head || !(*head)->next)
+	{
+		return (1);
+	}
 
-    /* Find the middle node using fast and slow pointers */
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
+	/* Find the middle node using fast and slow pointers */
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
 
-    /* Reverse the second half of the list */
-    while (slow) {
-        slow->next = prev;
-        prev = slow;
-        slow = slow->next;
-    }
+	/* Reverse the second half of the list */
+	while (slow)
+	{
+		slow->next = prev;
+		prev = slow;
+		slow = slow->next;
+	}
 
-    /* Compare the first and second halves */
-    slow = prev;
-    while (*head && slow) {
-        if ((*head)->n != slow->n) {
-            return (0);
-        }
-        *head = (*head)->next;
-        slow = slow->next;
-    }
+	/* Compare the first and second halves */
+	slow = prev;
+	while (*head && slow)
+	{
+		if ((*head)->n != slow->n)
+		{
+			return (0);
+		}
+	*head = (*head)->next;
+	slow = slow->next;
+	}
 
-    return (1);
+	return (1);
 }
-
