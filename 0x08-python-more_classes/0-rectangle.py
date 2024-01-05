d827c97747c382484ba9dc82852853ce61784213
Prof-Oj01 <ojoofe20@gmail.com>
2024-01-04 18:44:15 -0800

    simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Rectangle:
+   """
+   Represents a rectangle with width and height attributes.
+   """
+
+   def __init__(self):
+       """
+       Initializes a new Rectangle instance with default width and height of 0.
+       """
+       self.width = 0
+       self.height = 0
+
