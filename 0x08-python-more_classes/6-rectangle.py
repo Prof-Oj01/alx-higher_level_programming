916f4d5ad7f6994b2f3a5b82d5989ed78332f5e9
Prof-Oj01 <ojoofe20@gmail.com>
2024-01-05 17:38:52 -0800

    defined rectangle_6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,8 @@ class Rectangle:
     """
     This is a rectangle
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         if type(width) is not int:
             raise TypeError("width must be an integer")
@@ -20,6 +22,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -87,4 +90,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
